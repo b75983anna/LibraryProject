81fce12370d8875d395aa15a28fb723c1167c1a4
anna.borne <anna.borne@accenture.com>
2023-01-13 19:29:57 +0200

    controller & service update.

--- src/main/java/org/company/demo/lobrary/libraryProject/services/interfaces/BookService.java
+++ src/main/java/org/company/demo/lobrary/libraryProject/services/interfaces/BookService.java
@@ -3,10 +3,20 @@ package org.company.demo.lobrary.libraryProject.services.interfaces;
 import org.company.demo.lobrary.libraryProject.DTO.BookDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookService {
 
     public BookDTO addBook(BookDTO book);
 
     public List<BookDTO> getBooks();
+    public Optional<BookDTO> getBookByCode(Integer bookCode);
+
+    public BookDTO updateBook(BookDTO book, Integer id);
+
+    public void deleteBook(Integer id);
+
+    public List<BookDTO> getBooksByAuthor(String name);
+
+
 }
